52a6ef554cf3a0e8e227e7bcc95ea07f8e7cffc6
Anderson Castilho <leosilvacast@gmail.com>
2024-09-24 01:37:21 -0300

    Corrigindo bug catch

--- src/shared/exeption-filter/not-found-error.filter.ts
+++ src/shared/exeption-filter/not-found-error.filter.ts
@@ -1,15 +1,16 @@
-import { ArgumentsHost, ExceptionFilter } from "@nestjs/common";
-import { NotFoundError } from "../errors/not-found.error";
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { NotFoundError } from '../errors/not-found.error';
 
+@Catch(NotFoundError)
 export class NotFoundErrorFilter implements ExceptionFilter {
-    catch(exception: NotFoundError, host: ArgumentsHost) {
-        const ctx = host.switchToHttp()
-        const response = ctx.getResponse()
+  catch(exception: NotFoundError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
 
-        response.status(404).send({
-            statusCode: 404,
-            error: 'Not Found Error!',
-            message: exception.message
-        })
-    }
+    response.status(404).send({
+      statusCode: 404,
+      error: 'Not Found Error!',
+      message: exception.message,
+    });
+  }
 }
