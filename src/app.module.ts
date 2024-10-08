a14bf101caae40b8082ed9d21b8bc06ee22eb851
Anderson Castilho <leosilvacast@gmail.com>
2024-10-08 18:36:24 -0300

    Instaladno nestconfig para utilizar env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, ConfigModule.forRoot()],
 })
-export class AppModule {}
+export class AppModule { }
