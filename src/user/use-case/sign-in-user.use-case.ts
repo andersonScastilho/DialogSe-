3abc09bbb6da1d8be15add79d8584d7bed923c52
Anderson Castilho <leosilvacast@gmail.com>
2024-11-07 13:35:42 -0300

    Mudando o repository para buscar usuario

--- src/user/use-case/sign-in-user.use-case.ts
+++ src/user/use-case/sign-in-user.use-case.ts
@@ -1,24 +1,24 @@
 import { AuthService } from "@/auth/auth.service";
-import { IUserRepository } from "../database/repositories/user.repository";
 import { Inject } from "@nestjs/common";
 import { IHashProvider } from "@/shared/providers/hash-provider.interface";
 import { UnauthorizedError } from "@/shared/errors/unauthorized.error";
+import { IShowUserPerIdRepository } from "../database/repositories/show-user-per-id.repository";
 
 export class SignInUserUseCase {
     constructor(
         @Inject('HashProvider')
         private readonly hashProvider: IHashProvider,
-        @Inject('UserRepository')
-        private readonly userRepository: IUserRepository,
+        @Inject('ShowUserPerIdRepository')
+        private readonly showUserPerIdRepository: IShowUserPerIdRepository,
         private readonly authService: AuthService) { }
 
     async signIn(email: string, password_hash: string) {
-        const user = await this.userRepository.findByEmail(email)
+        const user = await this.showUserPerIdRepository.execute(email)
 
         const passwordIsMatch = await this.hashProvider.compare(password_hash, user.password_hash)
 
         if (!passwordIsMatch) {
-            throw new UnauthorizedError('Email ou senha incorreto.')
+            throw new UnauthorizedError('Incorrect email or password.')
         }
 
         const acessToken = await this.authService.generateTokenJwt(user.id)
